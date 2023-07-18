348a247ba7f28dd00dc366d5e67e879d127220dc
Ovilash Moitra <96794196+OvilashMoitra@users.noreply.github.com>
2023-07-18 21:12:48 +0600

    delete function added

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -1,17 +1,35 @@
 import React from 'react'
 import NavigationBar from '../components/NavigationBar/NavigationBar'
 import Footer from '../components/Footer/Footer'
-import { useGetSingleBookQuery } from '../redux/features/book/bookApiSlice'
-import { useParams } from 'react-router-dom'
+import { useDeleteBookMutation, useGetSingleBookQuery } from '../redux/features/book/bookApiSlice'
+import { useNavigate, useParams } from 'react-router-dom'
 import { useGetUserInfoQuery } from '../redux/features/user/userApiSlice'
+import swal from 'sweetalert'
 
 const BookDetails = () => {
-
+const navigate=useNavigate()
   const { id } = useParams()
   const token=localStorage.getItem("token")
   const { data: book, isLoading } = useGetSingleBookQuery(id)
-  const {data:userInfo,isLoading:userLoading}=useGetUserInfoQuery(token!)
-  if(isLoading || userLoading)<p>loading......</p>
+  const { data: userInfo, isLoading: userLoading } = useGetUserInfoQuery(token!)
+  const [deleteBook]=useDeleteBookMutation()
+  if (isLoading || userLoading) <p>loading......</p>
+  
+  const handleBookDelete = async(bookId) => {
+    const willDelete = await swal({
+      title: "Are you sure?",
+      text: "Are you sure that you want to delete this book?",
+      icon: "warning",
+      dangerMode: true,
+    });
+     
+    if (willDelete) {
+      swal("Deleted!", "Your book has been deleted!", "success");
+    }
+    console.log(bookId);
+    await deleteBook(bookId)
+navigate('/books')
+  }
 
   return (
     <div>
@@ -25,7 +43,7 @@ const BookDetails = () => {
             <p>Genre: {book?.data.genre}</p>
             {userInfo?.data?._id===book?.data.addedBy?<div className='my-4 flex justify-end'>
               <button className='btn  btn-warning'>Edit Book</button>
-              <button className='btn btn-error'>Delete book</button>
+              <button onClick={()=> handleBookDelete(book?.data?._id)}  className='btn btn-error'>Delete book</button>
             </div>:null}
             
         </div>
